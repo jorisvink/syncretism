b59fcb7083869c69f261409d8bdd03bb3b3fe193
Joris Vink <joris@sanctorum.se>
2026-01-28 20:56:55 +0100

    nyfe: sync with dd13ac23

--- nyfe/src/random.c
+++ nyfe/src/random.c
@@ -17,7 +17,9 @@
 #include <sys/param.h>
 #include <sys/types.h>
 
-#if defined(__APPLE__)
+#if defined(NYFE_PLATFORM_IOS)
+#include <Security/SecRandom.h>
+#elif defined(__APPLE__)
 #include <sys/random.h>
 #endif
 
@@ -119,19 +121,31 @@ random_rekey(void)
 #if !defined(NYFE_LIBRARY_ONLY)
 	/* Load in the entropy file, if its not present, complain. */
 	if ((fd = open(nyfe_entropy_path(), O_RDWR)) == -1) {
-		if (errno != ENOENT)
-			fatal("open(%s): %s", nyfe_entropy_path(), errno_s);
+		if (errno != ENOENT) {
+			nyfe_fatal("open(%s): %s",
+			    nyfe_entropy_path(), errno_s);
+		}
 		nyfe_output("warning: no entropy file present, "
 		    "only system entropy will be used\n");
 	} else {
 		if (nyfe_file_read(fd, add, sizeof(add)) != sizeof(add))
-			fatal("failed to read entropy file");
+			nyfe_fatal("failed to read entropy file");
 	}
 #endif
 
+#if defined(NYFE_PLATFORM_IOS)
+	if (SecRandomCopyBytes(kSecRandomDefault,
+	    sizeof(seed), seed) != errSecSuccess)
+		nyfe_fatal("SecRandomCopyBytes: failed");
+#elif defined(NYFE_PLATFORM_WINDOWS)
+	/* XXX */
+	if (RtlGenRandom(seed, sizeof(seed)) == 0)
+		nyfe_fatal("RtlGenRandom: failed");
+#else
 	/* Obtain some system entropy. */
 	if (getentropy(seed, sizeof(seed)) == -1)
-		fatal("getentropy: %s", errno_s);
+		nyfe_fatal("getentropy: %s", errno_s);
+#endif
 
 	/*
 	 * Derive key and nonce material using KMAC256 with the seed
@@ -167,7 +181,7 @@ random_rekey(void)
 	 */
 	if (fd != -1) {
 		if (lseek(fd, 0, SEEK_SET) == -1)
-			fatal("lseek: %s", errno_s);
+			nyfe_fatal("lseek: %s", errno_s);
 
 		nyfe_file_write(fd, ks, RANDOM_ADD_SIZE);
 
